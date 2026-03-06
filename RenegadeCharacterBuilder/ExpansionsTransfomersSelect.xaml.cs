7e1ea08268f9e42a9c8b3947aa08d0b52a5d04aa
Myrucj <myrucj@gmail.com>
2026-03-05 20:51:57 -0500

    Make expansions selection page for transformers

---
+++ RenegadeCharacterBuilder/ExpansionsTransfomersSelect.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RenegadeCharacterBuilder
+{
+    /// <summary>
+    /// Interaction logic for ExpansionsTransfomersSelect.xaml
+    /// </summary>
+    public partial class ExpansionsTransfomersSelect : Page
+    {
+        public ExpansionsTransfomersSelect()
+        {
+            InitializeComponent();
+        }
+    }
+}
