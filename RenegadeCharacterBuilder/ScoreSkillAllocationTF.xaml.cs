64d90bb17f0d03ca5d408dfb4c1819325ee89ae1
Myrucj <myrucj@gmail.com>
2026-04-04 00:59:26 -0400

    study and thinking no real big changes

--- RenegadeCharacterBuilder/ScoreSkillAllocationTF.xaml.cs
+++ RenegadeCharacterBuilder/ScoreSkillAllocationTF.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -16,34 +17,166 @@ namespace RenegadeCharacterBuilder
     /// <summary>
     /// Interaction logic for ScoreSkillAllocationTF.xaml
     /// </summary>
-    public partial class ScoreSkillAllocationTF : Page
+    public partial class ScoreSkillAllocationTF : Page, INotifyPropertyChanged
     {
-        public int pointBank = 9;
+        public event PropertyChangedEventHandler PropertyChanged;
+        private int _pointBank = 9;
         public int SocialSkilbank;
         public int StrengthSkilbank;
         public int SpeedlSkilbank;
         public int SmartslSkilbank;
 
         //for binding
-        public int Strength { get; set;} = 0;
-        public int Smarts { get; set; } = 0;
-        public int Speed { get; set; } = 0;
-        public int Social { get; set; }  = 0;
+
+        
+        private int _strength =1;
+        private int _speed = 1;
+        private int _smarts= 1;
+        private int _social= 1;
+
+        public int PointsBank
+        {
+            get => _pointBank;
+            set
+            {
+                _pointBank = value;
+                OnPropertyChanged(nameof(PointsBank));
+            }
+        }
+        public int Strength {
+            get => _strength; 
+                set { _strength = value;
+                OnPropertyChanged(nameof(Strength));
+                    } 
+        } 
+        public int Smarts
+        {
+            get => _smarts;
+            set { _smarts = value;
+                OnPropertyChanged(nameof(Smarts));
+
+            }
+        }
+        public int Speed 
+                { get => _speed;
+            set { _speed = value;
+                OnPropertyChanged(nameof(Speed));
+            } 
+        }
+        public int Social
+        {
+            get => _social;
+            set
+            {
+                _social = value;
+                OnPropertyChanged(nameof(Social));
+            }
+        }
 
         public ScoreSkillAllocationTF()
         {
             InitializeComponent();
             DataContext = this;
         }
+
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
         public  void AddToScore(object sender, RoutedEventArgs e)
         {
             // logic that binds button to skill
-            pointBank -= 1;
+            var btn = sender as Button;
+
+            string? stat = btn.Tag.ToString();
+            if(stat == null || PointsBank <= 0)
+            {
+                MessageBox.Show("No more points to spend");
+                return;
+            }
+            switch (stat)
+            {
+                case "Strength":
+                    Strength += 1;
+                    PointsBank -= 1;
+                    break;
+                case "Speed":
+                    Speed += 1;
+                    PointsBank -= 1;
+                    break;
+                case "Smarts":
+                    Smarts += 1;
+                    PointsBank -= 1;
+                    break;
+                case "Social":
+                    Social += 1;
+                    PointsBank -= 1;
+                    break;
+            }
+            
         }
         public  void SubtracFromoScore(object sender, RoutedEventArgs e)
         {
             // logic that binds button to skill
-            pointBank += 1;
+            var btn = sender as Button;
+
+            string? stat = btn.Tag.ToString();
+            if (stat == null)
+            {
+                MessageBox.Show("Null exception");
+                return;
+            }
+            switch (stat)
+            {
+                case "Strength":
+                    if (Strength == 1)
+                    {
+                        MessageBox.Show("Stat must be equal to at least 1");
+                        break;
+                    }
+                    else
+                    {
+                        Strength -= 1;
+                        PointsBank += 1;
+                        break;
+                    }
+                case "Speed":
+                    if (Speed == 1)
+                    {
+                        MessageBox.Show("Stat must be equal to at least 1");
+                        break;
+                    }
+                    else
+                    {
+                        Speed -= 1;
+                        PointsBank += 1;
+                        break;
+                    }
+                case "Smarts":
+                    if (Smarts == 1)
+                    {
+                        MessageBox.Show("Stat must be equal to at least 1");
+                        break;
+                    }
+                    else
+                    {
+                        Smarts -= 1;
+                        PointsBank += 1;
+                        break;
+                    }
+                case "Social":
+                    if (Social == 1)
+                    {
+                        MessageBox.Show("Stat must be equal to at least 1");
+                        break;
+                    }
+                    else
+                    {
+                        Social -= 1;
+                        PointsBank += 1;
+                        break;
+                    }
+            }
 
         }
         public  void AddToSkill(object sender, RoutedEventArgs e)
