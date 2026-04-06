3458f519af5078e6170fe89d86280cedabcb03a5
Myrucj <myrucj@gmail.com>
2026-04-05 20:09:53 -0400

    skeleton for scores

---
+++ RenegadeCharacterBuilder/Models/Transformers/ScoreTF.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace RenegadeCharacterBuilder.Models.Transformers
+{
+    internal class ScoreTF: INotifyPropertyChanged
+    {
+        private string name { get; init; }
+        private int currentRank;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public int CurrentRank
+        {
+            get => currentRank;
+            set
+            {
+                if (currentRank != value)
+                {
+                    currentRank = value;
+                    NotifyPropertyChanged(nameof(CurrentRank));
+                }
+            }
+        }
+
+        private void NotifyPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
