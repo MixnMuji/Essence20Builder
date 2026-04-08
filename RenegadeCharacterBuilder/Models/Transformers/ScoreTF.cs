10453d4196970389ff94fbb7eda914f1b8a28ab1
Myrucj <myrucj@gmail.com>
2026-04-07 23:51:40 -0400

    made modes for skill and score logic, next is viewmodel

--- RenegadeCharacterBuilder/Models/Transformers/ScoreTF.cs
+++ RenegadeCharacterBuilder/Models/Transformers/ScoreTF.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using System.Windows;
 
 namespace RenegadeCharacterBuilder.Models.Transformers
 {
@@ -9,6 +10,8 @@ namespace RenegadeCharacterBuilder.Models.Transformers
     {
         private string name { get; init; }
         private int currentRank;
+
+        private List<SkillTF> correspondingSkills = new();
 
         public event PropertyChangedEventHandler PropertyChanged;
         public int CurrentRank
@@ -24,9 +27,44 @@ namespace RenegadeCharacterBuilder.Models.Transformers
             }
         }
 
+        public bool CanIncreaseSkill()
+        {
+            int total = correspondingSkills.Sum(s => s.SkillScore);
+            return total < CurrentRank;
+        }
+        public void increaseSkill(SkillTF skill)
+        {
+            if (!CanIncreaseSkill())
+            {
+                throw new InvalidOperationException("Skill allocation exceeds essance score");
+            }
+            skill.SkillScore += 1;
+        }
+        public void decreaseSkill(SkillTF skill)
+        {
+            if(skill.SkillScore <= 0){
+                throw new InvalidOperationException("Skill Scores can not be less than zero");
+            }
+            skill.SkillScore -= 1;
+        }
+        public void AddToScore()
+        {
+            
+            CurrentRank += 1;
+        }
+        public void SubtractFromScore()
+        {
+            if (CurrentRank <=1)
+            {
+                throw new InvalidOperationException("Scores can not be have a lower value than 1");
+            }
+            CurrentRank -= 1;
+           
+        }
         private void NotifyPropertyChanged(string name)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
+       
     }
 }
---
+++ RenegadeCharacterBuilder/Models/Transformers/SkillTF.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+
+namespace RenegadeCharacterBuilder.Models.Transformers
+{
+    internal class SkillTF: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string name { get; set; }
+
+
+        private int _skillScore { get; set; }
+
+        public int SkillScore{
+            get => _skillScore;
+            set
+            {
+                _skillScore = value;
+                NotifyPropertyChanged(nameof(SkillScore));
+
+            }
+        }
+        private void NotifyPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+       
+
+        
+    }
+}
