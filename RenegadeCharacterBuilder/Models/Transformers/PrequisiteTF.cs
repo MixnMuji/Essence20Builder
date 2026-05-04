d15623c5aecc438ab414e61d849301a82108d4de
Myrucj <myrucj@gmail.com>
2026-05-04 16:55:40 -0400

    Made jsons for pages generalperk,focuses still need to be filled

--- RenegadeCharacterBuilder/Models/Transformers/PrequisiteTF.cs
+++ RenegadeCharacterBuilder/Models/Transformers/PrequisiteTF.cs
@@ -1,27 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using RenegadeCharacterBuilder.Models.Transformers.ModelsForState;
 
 namespace RenegadeCharacterBuilder.Models.Transformers
 {
     public class PrequisiteTF
     {
-        public List<int?> statRequirement { get; set; }
+        public int? statRequirement { get; set; } 
+        public List<string>? ApplicableStats { get; set; }
         public int? levelRequirement { get; set; }
 
         //basically our checker if characters can do a given stat
 
-        public bool CheckIfMet(int targetNumber, List<int?> statsTocheck, int? levelReq )
+        public bool CheckIfMet(int? targetNumber,List<string>? ApplicableStats, int? levelReq, TransfomersCharacterModel CurrentChar)
         {
-            if(statsTocheck != null)
-            {
-                foreach(int stat in statsTocheck)
-                {
-                    if (stat >= targetNumber)
-                        return true;
-                }
-            }
-            if (levelReq != null && levelReq >= targetNumber)
+            // if theres stats compare them to the stats of the character
+            // need to put skills in a list
+            // List<string> toCheck =
+
+
+            if (levelReq != null && CurrentChar.Level.Level >= targetNumber)
                 return true;
 
             return false;
