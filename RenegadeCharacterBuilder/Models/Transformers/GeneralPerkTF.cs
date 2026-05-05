b8dca8f5fea7858f8239a03f4bbe5d5d98df7c90
Myrucj <myrucj@gmail.com>
2026-05-04 21:18:58 -0400

    added to general perks json

--- RenegadeCharacterBuilder/Models/Transformers/GeneralPerkTF.cs
+++ RenegadeCharacterBuilder/Models/Transformers/GeneralPerkTF.cs
@@ -6,11 +6,13 @@ namespace RenegadeCharacterBuilder.Models.Transformers
 {
     public class GeneralPerkTF
     {
-        public String Name;
+        public string Name;
 
         public PrequisiteTF? requirment { get; set; }
 
         public string Text { get; set; }
+
+        public int timesSelectable { get; set; }
 
         // public abstract void ApplyEffects(TransfomersCharacterModel characterModel);
         
