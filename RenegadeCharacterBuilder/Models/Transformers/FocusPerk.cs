4aecbb239110d4aff62ff1392d41f3b7bc0e71c3
Myrucj <myrucj@gmail.com>
2026-05-07 20:44:09 -0400

    add focus new object and json

--- RenegadeCharacterBuilder/Models/Transformers/FocusPerk.cs
+++ RenegadeCharacterBuilder/Models/Transformers/FocusPerk.cs
@@ -6,11 +6,7 @@ namespace RenegadeCharacterBuilder.Models.Transformers
 {
     public class FocusPerk
     {
-        public string ParentRole { get; set; }
-
-        public string SubclassName { get; set; }
-
-        public int LevelRequirment { get; set; }
+ 
 
         public string AbilityName { get; set; }
 
---
+++ RenegadeCharacterBuilder/Models/Transformers/FocusTF.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace RenegadeCharacterBuilder.Models.Transformers
+{
+    public class FocusTF
+    {
+        public string subclassName { get; set; }
+
+        public string statToBoost { get; set; }
+
+        public List<string> skillsToboost { get; set;}
+        public List<FocusPerk> ranks {get; set; } 
+    }
+    //get focus with switch stament
+}
