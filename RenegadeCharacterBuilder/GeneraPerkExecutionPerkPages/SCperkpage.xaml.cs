45e47fee0ff9b80523f761f9b18862fa48dc70ad
Myrucj <myrucj@gmail.com>
2026-07-01 17:41:23 -0400

    Made size change vm and xaml page finishing up continue button

--- RenegadeCharacterBuilder/GeneraPerkExecutionPerkPages/SCperkpage.xaml.cs
+++ RenegadeCharacterBuilder/GeneraPerkExecutionPerkPages/SCperkpage.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Net.Security;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,17 +14,35 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RenegadeCharacterBuilder.Models.Transformers;
+using RenegadeCharacterBuilder.Models.Transformers.ModelsForState;
+using RenegadeCharacterBuilder.Models.Transformers.ViewModelsTF;
 
 namespace RenegadeCharacterBuilder.GeneraPerkExecutionPerkPages
 {
     /// <summary>
     /// Interaction logic for SCperkpage.xaml
     /// </summary>
-    public partial class SCperkpage : Page
+    public partial class SCperkpage : Page 
     {
+       public SCVM vm { get; set; }
+        
+        
+        
         public SCperkpage()
         {
+            vm = new SCVM();
             InitializeComponent();
+            DataContext = vm;
+        }
+
+        private void Continue(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                MessageBoxResult.Yes:
+                
+                MessageBoxResult.No)
+
         }
     }
 }
---
+++ RenegadeCharacterBuilder/Models/Transformers/ViewModelsTF/SCVM.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Controls;
+using RenegadeCharacterBuilder.Models.Transformers.ModelsForState;
+
+namespace RenegadeCharacterBuilder.Models.Transformers.ViewModelsTF
+{
+    public class SCVM
+    {
+   
+            public event PropertyChangedEventHandler PropertyChanged;
+            public ObservableCollection<string> sizes { get; set; } = ["Common", "Large", "Extended"];
+            public List<Altmode> choices { get; set; } = TFCharacterSession.CurrentTransfomer.Altmodes;
+
+            public ObservableCollection<string> afterSizeLogic { get; set; } = new();
+
+            private string chosenSize;
+
+            public string ChosenSize
+            {
+                get => chosenSize;
+                set
+                {
+                    chosenSize = value;
+                    OnPropertyChanged(ChosenSize);
+                }
+            }
+
+            private Altmode selection;
+
+            public Altmode Selection
+            {
+                get => selection;
+                set
+                {
+                    selection = value;
+                    FindlegalSizes();
+                    OnPropertyChanged(nameof(Selection));
+
+                }
+            }
+
+
+
+            public void FindlegalSizes()
+            {
+                afterSizeLogic = sizes;
+                // set the collections equal because if we keep sizes imutable we're fine
+
+                // we need the transfoerms botsize index
+                // then we can say to remove inxe of observable collection
+
+                string[] botSizes = ["Common", "Long", "Huge"];
+
+                var targetOrign = TFCharacterSession.CurrentTransfomer.Origns[0];
+
+                string sizeToRemove = targetOrign.BotMode.Size;
+
+                int removeIndex = Array.IndexOf(botSizes, sizeToRemove);
+
+                afterSizeLogic.RemoveAt(removeIndex); // since they share the same progresion this will remove the size of the same index, common and common are at index 0
+
+
+
+            }
+
+            protected void OnPropertyChanged([CallerMemberName] string name = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            }
+        
+        }
+    }
+
+
