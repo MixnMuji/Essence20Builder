8090f9bf50b3fe61574a3f29170dfc3a831149a3
Myrucj <myrucj@gmail.com>
2026-07-19 23:26:35 -0400

    Ran questPDF test case

--- RenegadeCharacterBuilder/startpage.xaml.cs
+++ RenegadeCharacterBuilder/startpage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Text;
@@ -12,7 +14,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using RenegadeCharacterBuilder.Models.Transformers;
-
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
 
 namespace RenegadeCharacterBuilder
 {
@@ -24,7 +28,8 @@ namespace RenegadeCharacterBuilder
        
         public startpage()
         {
-            
+            QuestPDF.Settings.License = LicenseType.Community;
+
             InitializeComponent();
             ComboBox1.ItemsSource = Enum.GetValues(typeof(GameSelected));
            
@@ -78,6 +83,52 @@ namespace RenegadeCharacterBuilder
                     NavigationService.Navigate(new FocusPerks());
                     break;
                 case "PDFTest":
+                    string path = System.IO.Path.Combine("SavedCharacters",
+                                  "Transformers",
+                                  "Pdf",
+                                  "TestCase.pdf"
+                     );
+                    Directory.CreateDirectory(System.IO.Path.GetDirectoryName(path));
+
+                    Document.Create(Container =>
+                    {
+                        Container.Page(page =>
+                        {
+                            page.Size(PageSizes.A4);
+                            page.Margin(2, Unit.Centimetre);
+                            page.PageColor(QuestPDF.Helpers.Colors.White);
+                            page.DefaultTextStyle(x => x.FontSize(20));
+
+                            page.Header()
+            .Text("Hello PDF!")
+            .SemiBold().FontSize(36).FontColor(QuestPDF.Helpers.Colors.Blue.Medium);
+
+                            page.Content()
+                                .PaddingVertical(1, Unit.Centimetre)
+                                .Column(x =>
+                                {
+                                    x.Spacing(20);
+
+                                    x.Item().Text(Placeholders.LoremIpsum());
+                                    x.Item().Image(Placeholders.Image(200, 100));
+                                });
+
+                            page.Footer()
+                                .AlignCenter()
+                                .Text(x =>
+                                {
+                                    x.Span("Page ");
+                                    x.CurrentPageNumber();
+                                });
+                        });
+                    }).GeneratePdf(path);
+
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = path,
+                        UseShellExecute = true
+                    });
+                    
                    
                     break;
             }
