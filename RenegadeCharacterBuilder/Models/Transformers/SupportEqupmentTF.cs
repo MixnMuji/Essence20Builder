d495c6345fb6d0be71ec0c4fe7329f324857e861
Myrucj <myrucj@gmail.com>
2026-08-16 17:58:19 -0400

    added support equiment model and json

---
+++ RenegadeCharacterBuilder/Models/Transformers/SupportEqupmentTF.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RenegadeCharacterBuilder.Models.Transformers
+{
+    public class SupportEqupmentTF
+    {
+        public string name {get;set;}
+        public string prereq { get; set; }
+        public string altmodeEffect { get; set; }
+
+        public string botmodeEffect { get; set; }
+    }
+}
