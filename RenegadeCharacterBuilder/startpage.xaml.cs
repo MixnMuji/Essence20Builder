2124213c54aec3fd9718f15eba73eb93280fbd05
Myrucj <myrucj@gmail.com>
2026-09-05 16:05:47 -0400

    Startpage clean up: removed devskip Influence Page: removed scrollbar added text wrapping, added correct formate for up and downshift Origins json added mode names will help for later in aplication,added textwrapping got ride of scroller, removed old alliegence selector

--- RenegadeCharacterBuilder/startpage.xaml.cs
+++ RenegadeCharacterBuilder/startpage.xaml.cs
@@ -63,76 +63,8 @@ namespace RenegadeCharacterBuilder
             }
         }
 
-        private void DevMode(object sender, RoutedEventArgs e)
-        {
-            if(devSelection.SelectedItem == null)
-            {
-                MessageBox.Show("Need choice");
-                return;
-            }
-            var page = devSelection.SelectionBoxItem;
-            switch (page)
-            {
-                case "Roles":
-                    NavigationService.Navigate(new RolesTF());
-                    break;
-                case "Scores":
-                    NavigationService.Navigate(new ScoreSkillAllocationTF());
-                    break;
-                case "Focus":
-                    NavigationService.Navigate(new FocusPerks());
-                    break;
-                case "PDFTest":
-                    string path = System.IO.Path.Combine("SavedCharacters",
-                                  "Transformers",
-                                  "Pdf",
-                                  "TestCase.pdf"
-                     );
-                    Directory.CreateDirectory(System.IO.Path.GetDirectoryName(path));
+       
 
-                    Document.Create(Container =>
-                    {
-                        Container.Page(page =>
-                        {
-                            page.Size(PageSizes.A4);
-                            page.Margin(2, Unit.Centimetre);
-                            page.PageColor(QuestPDF.Helpers.Colors.White);
-                            page.DefaultTextStyle(x => x.FontSize(20));
-
-                            page.Header()
-            .Text("Hello PDF!")
-            .SemiBold().FontSize(36).FontColor(QuestPDF.Helpers.Colors.Blue.Medium);
-
-                            page.Content()
-                                .PaddingVertical(1, Unit.Centimetre)
-                                .Column(x =>
-                                {
-                                    x.Spacing(20);
-
-                                    x.Item().Text(Placeholders.LoremIpsum());
-                                    x.Item().Image(Placeholders.Image(200, 100));
-                                });
-
-                            page.Footer()
-                                .AlignCenter()
-                                .Text(x =>
-                                {
-                                    x.Span("Page ");
-                                    x.CurrentPageNumber();
-                                });
-                        });
-                    }).GeneratePdf(path);
-
-                    Process.Start(new ProcessStartInfo
-                    {
-                        FileName = path,
-                        UseShellExecute = true
-                    });
-                    
-                   
-                    break;
-            }
-
-        }
+        
     }
 }
