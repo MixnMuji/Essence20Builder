caadc269fe773fb48ded57e47d32586328ae3efd
Myrucj <myrucj@gmail.com>
2026-09-07 17:02:06 -0400

    Overhaulled Origin Page to display To be listbox that shows all will be better for future displays next need to overhall influences to be the same with this new design all origins are selected on left side of page and right displays all the details in the future when expansion are added space is saved.

--- RenegadeCharacterBuilder/OrignSelectTf.xaml.cs
+++ RenegadeCharacterBuilder/OrignSelectTf.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Text;
 using System.Text.Json;
@@ -21,41 +22,81 @@ namespace RenegadeCharacterBuilder
     /// <summary>
     /// Interaction logic for OrignSelectTf.xaml
     /// </summary>
-    public partial class OrignSelectTf : Page
+    public partial class OrignSelectTf : Page, INotifyPropertyChanged
     {
         public List<TransformersOrign> tfOrgins { get; set; }
+        public List<string> OriginNames { get; set; } = new();
+        // we need the names of each orign,
+        // need to say that if the selected item is chosen display the origin with its name so we can say tforigns is or pointer
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string _selectedItem;
+        public string SelectedItem
+        {
+            get => _selectedItem;
+            set
+            {
+                _selectedItem = value;
+                NotifyPropertyChanged(nameof(SelectedItem));
+                UpdateViewToMatchOrigin();
+            }
+                    
+        }
+        private TransformersOrign _currentView;
+        public TransformersOrign CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                NotifyPropertyChanged(nameof(CurrentView));
+            }
+        }
         public OrignSelectTf()
         {
             InitializeComponent();
             LoadOrigins();
+            GetNames();
             DataContext = this;
    
         }
-        void LoadOrigins()
+
+        public void GetNames()
+        {
+            foreach( var x in tfOrgins)
+            {
+                OriginNames.Add(x.Name);
+            }
+        }
+
+        public void UpdateViewToMatchOrigin()
+        {
+            CurrentView = tfOrgins.FirstOrDefault(x => x.Name == SelectedItem);
+
+        }
+        public void LoadOrigins()
         {
             string path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Jsoncollection", "TransformersJsons", "Origins.json");
             string json = File.ReadAllText(path);
             var originRoot = JsonSerializer.Deserialize<TFOriginsRoot>(json);
             tfOrgins = originRoot.Origins;
         }
-       private void OriginSelected(object sender, RoutedEventArgs e)
-        {
-            RadioButton rb = sender as RadioButton;
-            var selectedOrigin = rb.DataContext as TransformersOrign;
-            TFCharacterSession.CurrentTransfomer.Origns.Add(selectedOrigin);
-            TFCharacterSession.CurrentTransfomer.Altmodes.Add(selectedOrigin.AltMode);
-        }
+       
 
         private void ToInfluences(object sender, RoutedEventArgs e)
         {
-      
-           if (TFCharacterSession.CurrentTransfomer.Origns == null)
+
+            if (TFCharacterSession.CurrentTransfomer.Origns == null)
             {
                 MessageBox.Show("You must pick an Origin Before continuing");
 
             }
             else
+                TFCharacterSession.CurrentTransfomer.Origns[0] = CurrentView;
             NavigationService.Navigate(new TFInfluencePage());
+        }
+        private void NotifyPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
